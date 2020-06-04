4b36d0b34c1ca22e4985cc43c2baf6ba33d5c719
Heny11110 <henoke.tadesse@gmail.com>
2020-06-04 14:29:30 -0400

    Update Quiz.js

--- src/Quiz.js
+++ src/Quiz.js
@@ -9,8 +9,8 @@ class Quiz extends React.Component{
  }
  render() {
    return (
-     <div className="QuizQuestion"></div>
+     <div className="QuizQuestion">{quizData.quiz_questions[0].instruction_text}</div>
    );
  }
 }
-export default Quiz;
+export default Quiz
